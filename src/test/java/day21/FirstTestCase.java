ee956396ab3ee6d91b2e14273962e9500b2c45bd
mahdiajebin <mahdiajebin@gmail.com>
2024-07-01 23:43:46 -0400

    Initial commit

---
+++ src/test/java/day21/FirstTestCase.java
@@ -0,0 +1,42 @@
+package day21;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.SQLOutput;
+
+/*
+Test Case
+-----------
+1) Launch browser(chrome)
+2) Open URL https://demo.opencart.com/
+3) validate title should be "Your Store"
+4) close browser
+
+
+ */
+public class FirstTestCase {
+
+    public static void main(String[] args) {
+
+
+      //  1) Launch browser(chrome)
+       // ChromeDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
+       // 2) Open URL https://demo.opencart.com/
+        driver.get("https://demo.opencart.com/");
+       //3) validate title should be "Your Store"
+        // ---- capture actual title and compare with test title
+        String actTitle= driver.getTitle();
+        if(actTitle.equals("Your Store")){
+            System.out.println("Test passed");
+        }else {
+
+            System.out.println("Test Failed");
+
+        }
+
+        //Close Browser
+        driver.close();
+    }
+}
